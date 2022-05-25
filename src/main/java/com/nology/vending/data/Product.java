ad59ecd2d4f1b65f10df20a06f1f3dcbf9e18f27
ash-p98 <32458304+ash-p98@users.noreply.github.com>
2022-05-25 12:17:18 +0100

    Product getters and setters

--- src/main/java/com/nology/vending/data/Product.java
+++ src/main/java/com/nology/vending/data/Product.java
@@ -1,9 +1,14 @@
 package com.nology.vending.data;
 
+import java.util.Objects;
+import java.util.StringJoiner;
+
 public class Product {
     private String id;
     private String name;
+    private String category;
     private int qty;
+    private float price;
 
     public String getId() {
         return id;
@@ -33,6 +38,35 @@ public class Product {
         this.price = price;
     }
 
-    private float price;
+    public String getCategory() {
+        return category;
+    }
 
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString(){
+        return new StringJoiner(",", Product.class.getSimpleName() + "[", "]")
+                .add("id='" + id + "'")
+                .add("name='" + name + "'")
+                .add("price='" + price + "'")
+                .add("qty='" + qty + "'")
+                .add("category='" + category + "'")
+                .toString();
+    }
 }
