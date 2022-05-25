9ec5ad4effa1acbbb254d53e8289a91377244c01
ash-p98 <32458304+ash-p98@users.noreply.github.com>
2022-05-25 11:30:39 +0100

    database

--- src/main/java/com/nology/vending/data/Product.java
+++ src/main/java/com/nology/vending/data/Product.java
@@ -1,4 +1,38 @@
 package com.nology.vending.data;
 
 public class Product {
+    private String id;
+    private String name;
+    private int qty;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    private float price;
+
 }
